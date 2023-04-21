3ba50475e583ec2ba7d25907dd794c08a0a4e8b4
JeanLeLoup <hebertjeanl@hotmail.com>
2023-04-21 15:48:52 -0400

    Ajout mainMenu
    Ajout du menu principal.

---
+++ TP1-Prog2.py
@@ -0,0 +1,17 @@
+#import os
+#import pandas as pd
+
+
+def mainMenu():
+   print("*****************************************************************************")
+   print("1-Afficher l’ensemble des aliments depuis le fichier Nutrition.CSV       ****")
+   print("2-Afficher les aliments en fonction d’une valeur nutritive à la fois     ****")
+   print("3-Afficher les valeurs nutritives d’un aliment (Recherche par ID)        ****")
+   print("4-Modifier une valeur nutritive d’un aliment par ID )                    ****")
+   print("5-Ajouter un aliment                                                     ****")
+   print("6-Quitter                                                                ****")
+   print("*****************************************************************************")
+
+#------------------------------Début Code----------------------------------#
+
+mainMenu()
